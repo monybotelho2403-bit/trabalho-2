90f423bf316e3c5fc79b3552aae27564f0e9aea4
monybotelho2403-bit <monybotelho2403@hotmail.com>
2026-03-23 22:33:45 -0300

    resolvendo conflitos

---
+++ cad-usuario.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro de Usuário</title>
+    <link rel="stylesheet" href="dash.css">
+</head>
+<body>
+    <main>
+        <section class= "card-form">
+            <div class= "form-header">
+                <h2>Cadastro de Usuário</h2>
+                <p>Preencha os campos abaixo para criar um novo usuário no sitema</p>
+            </div>
+            <form action="insere-usuario.php" method="POST">
+                <div class= "form-group">
+                    <label for= "nome">Nome Completo</label>
+                    <input type= "nome" id= "nome" name= "nome" placeholder= "Nome completo" required>
+            <form action="insere-usuario.php" method="POST">
+                <div class= "form-group">
+                      <label for= "email">E-mail</label>
+                      <input type= "email" id= "email" name ="email" placeholder= "nome@empresa.com" required>
+                </div>
+                <div class="form-row">
+                    <div class="form-group flex-1">
+                        <label for= "senha">Senha</label>
+                        <input type="password" id="senha" name="senha" required>
+                    </div>
+                    <div class="form-group flex-1">
+                        <label for= "nivel">Nivel</label>
+                        <select id="nivel" name="nivel">
+                            <option value="1">Usuário</option>
+                            <option value="2">Administrador</option>
+                        </select>    
+                    </div>
+                </div>
+                <div class="form-actions">
+                    <button type="submit" class="btn-save">Finalizar Cadastro</button>
+                    <a href="index.php" class="btn-cancel">Cancelar</a>
+                </div>    
+            </form>
+        </section>
+        </main>
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,15 @@
+<nav>
+
+<ul>
+<li><a href="index.php">Início</a></li>
+<li><a href="usuarios.php">Usuários</a></li>
+<li><a href="#">Categorias</a></li>
+<li><a href="#">Postagens</a></li>
+</ul>
+
+<div class="perfil-usuario">
+<img src="https://ui-avatars.com/api/?name=Monyelle+Botelho&background=008080&color=fff">
+<span>Monyelle Botelho</span>
+</div>
+
+</nav>
